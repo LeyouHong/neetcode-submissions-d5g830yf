06693131f18eeac102f586061edb4401ab1d3cea
Leyou Hong <leyouhong1991@gmail.com>
2026-04-17 22:12:52 -0700

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.go
@@ -0,0 +1,32 @@
+func longestCommonSubsequence(text1 string, text2 string) int {
+    m, n := len(text1), len(text2)
+
+    dp := make([][]int, m+1)
+    for i := 0; i < m+1; i++ {
+        dp[i] = make([]int, n+1)
+    }
+    //go default value is 0
+
+    for i := 1; i < m+1; i++ {
+        for j := 1; j < n+1; j++ {
+            if text1[i-1] == text2[j-1] {
+                dp[i][j] = dp[i-1][j-1] + 1
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+
+    return dp[m][n]
+}
+
+/*
+    
+     "" a b c e e
+   ""0  0 0 0 0 0
+   a 0  1 1 1 1 1
+   e 0  1 1 1 2 2 
+   e 0  1 1 2 3 3
+  
+
+*/
