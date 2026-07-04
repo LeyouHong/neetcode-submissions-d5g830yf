cbb6bed47a0aae71b5aab3c99b1fa14279a3d252
Leyou Hong <leyouhong1991@gmail.com>
2026-07-04 12:59:14 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        const map = new Map();
+        for (let c of s) {
+            map.set(c, (map.get(c) || 0) + 1)
+        }
+
+        for (let c of t) {
+            if(!map.has(c)) return false;
+            map.set(c, map.get(c)-1);
+            if (map.get(c) < 0) return false;
+        }
+
+        return true;
+    }
+}
